70cc2fd54c5648d4eab0ce4004e7ed88364c586c
David Jonas <davjon01@easv365.dk>
2025-05-30 18:26:46 +0200

    removed unused code

--- src/main/java/exam_easv_belman/BLL/exceptions/InvalidCredentialsException.java
+++
@@ -1,7 +0,0 @@
-package exam_easv_belman.BLL.exceptions;
-
-public class InvalidCredentialsException extends Exception {
-    public InvalidCredentialsException(String message) {
-        super(message);
-    }
-}
